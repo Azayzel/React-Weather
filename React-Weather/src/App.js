10525e4a1aab5eed070ab9ccd369e0499971af48
Josh Lavely <josh.lavely@gmail.com>
2018-05-16 22:04:59 -0400

    Added Bootstrap for styling.

--- React-Weather/src/App.js
+++ React-Weather/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './sun.gif';
 import './App.css';
+import './components/getZip'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import GetZip from './components/getZip';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <header className="page-header">
+            <img src={logo} className="img-responsive" alt="logo"/>
+            <h1 className="App-title">React Weather App</h1>
+          </header>
+        </div>
+        <p className="App-intro"></p>
+        <GetZip/>
       </div>
     );
   }
---
+++ React-Weather/src/components/getZip.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class GetZip extends Component {
+    render() {
+        return (
+            <div className="col-lg-12">
+                <input className="form-control" placeholder="Enter your ZIP"/>
+            </div>
+        )
+    }
+
+}
+export default GetZip;
